8bacb01796010cceb1a2e9b9aac02283660d6cdd
nicolasdelisee <nicolas.delisee3@gmail.com>
2022-11-15 10:52:49 +0100

    Quest 2 completed

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,14 +14,19 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/movies", movieHandlers.postMovie);
 
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
     console.log(`Server is listening on ${port}`);
-  }
-});
+}});
--- movieHandlers.js
+++ movieHandlers.js
@@ -1,47 +1,51 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
+const database = require("./database.js");
 
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("SELECT * FROM movies")
+    .then(([movies])=> {
+      res.json(movies);
+    })
+    .catch((err)=> {
+        console.log(err);
+        res.status(500).send("Error retrieving movies from database");
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database 
+    .query(`SELECT * FROM movies WHERE id = ?`, [id])
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.json(movies[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Error retrieving movies from database")
+    });
 };
+
+const postMovie = (req, res) => {
+  const {title, director, year, color, duration} = req.body;
+
+  database
+    .query("INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?", [title, director, year, color, duration])
+    .then(([result]) => {
+      console.log(result.insertId);
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    })
+  };
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
